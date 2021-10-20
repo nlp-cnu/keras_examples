bee6dfc88e2d363ab341f17f6e503f74bedc72d1
henryst57 <samuel.henry@cnu.edu>
2021-10-20 10:09:40 -0400

    updated DataGenerator with MAX_LENGTH

--- DataGenerator.py
+++ DataGenerator.py
@@ -7,12 +7,15 @@ import numpy as np
 # in terms of training time because it allows for variable size batches. (depending
 # on the maximum length of sequences in the batch)
 class DataGenerator(tf.keras.utils.Sequence):
-    def __init__(self, x_set, y_set, batch_size, tokenizer, shuffle=True):
+    MAX_LENGTH = 512
+    
+    def __init__(self, x_set, y_set, batch_size, tokenizer, shuffle=True, max_length=MAX_LENGTH):
         self.x = x_set
         self.y = y_set
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.tokenizer = tokenizer
+        self._max_length = max_length
 
     def __len__(self):
         return int(np.ceil(len(self.x) / self.batch_size))
@@ -21,8 +24,8 @@ class DataGenerator(tf.keras.utils.Sequence):
         batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
         batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
 
-        #TODO - max length may be > 512 depending on the classifier
-        tokenized = self.tokenizer(batch_x, padding=True, truncation=True, max_length=512, return_tensors='tf')
+        #Tokenize the input
+        tokenized = self.tokenizer(batch_x, padding=True, truncation=True, max_length=self._max_length, return_tensors='tf')
                                                      
         return (tokenized['input_ids'], tokenized['attention_mask']), batch_y
     
