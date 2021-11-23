7237de41a04c505902b7a096ee19bf6afa5d533e
henryst57 <samuel.henry@cnu.edu>
2021-11-23 14:01:32 -0500

    added comments to Dataset

--- Dataset.py
+++ Dataset.py
@@ -1,23 +1,24 @@
-#import os
-
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
-#from sklearn.model_selection import train_test_split
-import sklearn.model_selection
+#import sklearn.model_selection
 from abc import ABC, abstractmethod
 
 #import preprocessor as p
 #from sklearn.utils import class_weight
 
+#TODO - Should I have a default seed value here?
 SEED = 3
 
 #Abstract dataset class
 class Dataset(ABC):
     
     @abstractmethod
-    def __init__(self, seed=SEED, validation_set_size=0):  # use_all_data=False,
-        #validation_set_size is the percentage to use for validation set (e.g. 0.2 = 20%
+    def __init__(self, seed=SEED, validation_set_size=0):
+        """
+        Constructor for a Dataset
+        validation_set_size is the percentage to use for validation set (e.g. 0.2 = 20%
+        """
         self.seed = seed
         self._val_set_size = validation_set_size
         self._train_X = None
@@ -26,30 +27,38 @@ class Dataset(ABC):
         self._val_Y = None
 
 
-    #I should also maintain the class ratio during the val/train split...is that happening here?
-    # .... I should check the sklearn implementation
     def _training_validation_split(self, data, labels):
-        # Split data
+        """
+        Performs a stratified training-validation split
+        """   
+        # Error Checking
         if (self._val_set_size >= 1):
             raise Exception("Error: test set size must be greater than 0 and less than 1")
+        
         if (self._val_set_size > 0):
-           self._train_X, self._val_X, self._train_Y, self._val_Y = sklearn.model_selection.train_test_split(data, labels, test_size=self._val_set_size, random_state=self.seed)
+            #Split the data - this automatically does a stratified split
+            # meaning that the class ratios are maintained
+            self._train_X, self._val_X, self._train_Y, self._val_Y = sklearn.model_selection.train_test_split(data, labels, test_size=self._val_set_size, random_state=self.seed)
         else:
+            # set the data to unsplit
             self._train_X = data
             self._train_Y = labels
             self._val_X = None
             self._val_Y = None
-           
+
+            
     def get_train_data(self):
         if self._train_X is None or self._train_Y is None:
             raise Exception("Error: train data does not exist, you must call _training_validation_split after loading data")
         return self._train_X, self._train_Y
 
+    
     def get_validation_data(self):
         if self._val_X is None or self._val_Y is None:
             raise Exception("Error: val data does not exist, you must specify a validation split percent")
         return self._val_X, self._val_Y
 
+    
     #You can tweek this however you want
     def preprocess_data(self, data):
         # preprocess tweets to remove mentions, URL's
@@ -65,7 +74,9 @@ class Dataset(ABC):
 
     def get_train_class_weights(self):
         return self.class_weights
-    
+
+
+    #TODO - see if class weights is working correctly
     def _determine_class_weights(self):
         # determine class weights
         self.class_weights = class_weight.compute_class_weight(
@@ -75,8 +86,10 @@ class Dataset(ABC):
             #y=self._train_Y.argmax(axis=1) #TODO -- use this (or something like it) for multiclass problems
         )
         self.class_weights = dict(enumerate(self.class_weights))
-    
-    #TODO - this is based on Max's code and has some hardcoded values - make it more generic
+
+
+        
+#TODO - this is based on Max's code and has some hardcoded values - make it more generic
 class MultiLabel_Text_Classification_Dataset(Dataset):
     def __init__(self, data_file_path, text_column_name=None, label_column_name=None, seed=SEED, validation_set_size=0):
         Dataset.__init__(self, seed=seed, validation_set_size=validation_set_size)
@@ -102,7 +115,7 @@ class MultiLabel_Text_Classification_Dataset(Dataset):
         # data = self.preprocess_data(raw_data)
 
         self._training_validation_split(data, labels)
-        # self._determine_class_weights() #TODO - re-implement this
+        # self._determine_class_weights() 
 
 
 #Loads data in which there is a single (categorical) label column (e.g. class 0 = 0, class 2 = 2)
@@ -126,8 +139,9 @@ class MultiClass_Text_Classification_Dataset(Dataset):
         data = df[text_column_name].values.tolist()
         #data = self.preprocess_data(raw_data)
         
-        self._training_validation_split(data, labels)
-        #self._determine_class_weights() #TODO - re-implement this
+        self._training_validation_split(d
+                                        ata, labels)
+        #self._determine_class_weights() 
     
 #Load a data and labels for a text classification dataset
 class Binary_Text_Classification_Dataset(Dataset):
@@ -192,10 +206,10 @@ class Binary_Text_Classification_Dataset(Dataset):
         #data = self.preprocess_data(raw_data)
         
         self._training_validation_split(data, labels)
-        #self._determine_class_weights() #TODO - re-implement this
+        #self._determine_class_weights()
         
 
-# TODO -- This will work for multi-class problems, and I think it works for mult-label problems
+# TODO -- This will work for multi-class problems, and I think it works for multi-label problems
 # TODO - this currently uses hard-coded values so its functionality is limited. It serves as a template though
 # TODO -- may need to expand for different format types. Right now it is for span start and span end format types
 class Token_Classification_Dataset(Dataset):
